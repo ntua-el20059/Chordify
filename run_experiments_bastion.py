44a68eeb602772e8b1a9fd1ed9fdab52f4fa538c
Klarinetos Plokamias <sgkiouleas.com>
2025-03-18 03:00:37 +0200

    Marku Bocari

--- run_experiments_bastion.py
+++ run_experiments_bastion.py
@@ -107,8 +107,8 @@ def main():
             print(f"Failed to git pull on {hostname}. Exiting...")
             sys.exit(1)
 
-    consistency_types = ["eventual"]
-    replication_factors = [3]
+    consistency_types = ["linearizability", "eventual"]
+    replication_factors = [1, 3, 5]
     for consistency in consistency_types:
         for replication in replication_factors:
             run_experiment(args.base_hostname, consistency, replication)
